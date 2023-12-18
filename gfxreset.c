9aac0911271bbae4403aba4c15e3153e7072ec48
Adam <adam@polystream.com>
2023-12-18 18:38:55 +0000

    - when a Tek4404 graphics app does not cleanly exit, it can leave keyboard 100% disabled..

---
+++ gfxreset.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <graphics.h>
+
+int main(argc, argv)
+int argc;
+char **argv;
+{
+    InitGraphics(FALSE);
+
+    EventDisable();
+    SetKBCode(1);
+
+  return 0;      	
+}
