ded82a76c0794d373c755e00399013a18461764a
Adam <adam@polystream.com>
2024-01-08 18:25:59 +0000

    - clang needs pack pragma

--- ph.h
+++ ph.h
@@ -1,6 +1,8 @@
 /* tek4404 executable header */
 
+#ifdef __clang__
 #pragma pack(push, 1)
+#endif
 
 /* probably 64 bytes, big endian */
 typedef struct
@@ -23,13 +25,14 @@ typedef struct
   short commentsize;
   short rcssize;
 
-  short flags;    // 0x8000 = no mc68881   0x0040 = core dump, 0x0020 = not clear BSS
+  short flags;    /* 0x8000 = no mc68881   0x0040 = core dump, 0x0020 = not clear BSS */
   
   int unknown2[3];
   
-  int data2;      // eg 0x00020000
-  int data3;      // eg 0x00040000
+  int data2;      /* eg 0x00020000 */
+  int data3;      /* eg 0x00040000 */
 } PH;
 
+#ifdef __clang__
 #pragma pack(pop)
-
+#endif
