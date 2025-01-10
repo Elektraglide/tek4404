031a2588a2d7eb7bceca114bd724f3f1e6c49091
Adam <adam@light-up.co.uk>
2025-01-10 08:10:06 +0000

    - add entrypoint for undocumented system_control() that can make you the controlling terminal

--- kernasm.s
+++ kernasm.s
@@ -1,7 +1,20 @@
 	lib sysdef
 
+	global _system_control
+	
+; this is just missing from clibs
+	text
+_system_control	link a6,#0
+	movem.l d2-d7/a2-a5,-(sp)
+	move.l 8(a6),d0
+	sys system_control
+	movem.l (sp)+,d2-d7/a2-a5
+	unlk a6
+	rts
+
 	global _kernscsidump
 	
+; expects #0x1084 or #0x1b1b824 magic values for choosing message
 	text
 _kernscsidump	link a6,#0
 	movem.l d2-d7/a2-a5,-(sp)
@@ -71,3 +84,36 @@ _kernexec	link a6,#0
 	rts
 
 	end
+
+; need a way of validating this absolute address has not moved
+;
+add_custom_map EQU $fe6a
+
+	global _kernmapper
+
+	text
+_kernmapper	link a6,#0
+
+  lea fpu_seg(pc),a0
+; if we could pass arguments to kernexec()  (via usarg0?)
+;  move.l 8(a6),(a0)
+;  move.l 12(a6),4(a0)
+;  move.l 16(a6),d0
+;  move.w d0,8(a0)
+  bsr add_custom_map
+  move.l (a0),d0
+  rts
+  
+; Uniflex standard umdep_segs
+; 0x007e0000  0x00600000  videoram
+; 0x007de000  0x00000000  shared_page0 (phys(2) is unsupported)
+; 0x007dd000  0x00000000  shared_page1 (phys(3) is unsupported)
+; 0x007dc000  0x007ba000  rtc
+;
+
+fpu_seg    EQU     *
+        dc.l 0x007db000
+        dc.l 0x0078c000   ;fpu
+        dc.w 1
+
+mappersize    EQU     *-_kernmapper
