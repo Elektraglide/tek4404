119eaa341ba9a5df3e02b8f0ded4040deee22042
AdamB <adam@light-up.co.uk>
2025-02-07 08:39:30 +0000

    - added lookup for shifted keys - added ifdef TESTING test harness

--- wmgr/events.c
+++ wmgr/events.c
@@ -1,12 +1,25 @@
-//
-//  events.c
-//  tek4404_utilities
-//
-//  Created by Adam Billyard on 05/02/2025.
-//
-
 #include <stdio.h>
 #include <graphics.h>
+
+unsigned char shifted[128] = 
+{
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\"',
+  '\0','\0','\0','\0', '<', '_', '>', '?',
+   ')', '!', '@', '#', '$', '%', '^', '&',
+   '*', '(','\0', ':','\0', '+','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0', '{', '`', '}','\0','\0',
+  '\0', 'A', 'B','C','D','E','F','G',
+  'H','I','J','K','L','M','N','O',
+  'P','Q','R','S','T','U','V','W',
+  'X','Y','Z','~','\0','\0','\0',0x7f
+};
 
 unsigned char keymap[256];
 unsigned long timestamp = 0;
@@ -23,9 +36,11 @@ int maxlen;
   int ecount;
   union EVENTUNION ev;
   unsigned long timelo,timehi;
+  int total = 0;
+  unsigned char ch;
   
   ecount = EGetCount();
-  while (ecount-- > 0 && maxlen)
+  while (ecount-- > 0 && total < maxlen)
   {
     ev.evalue = EGetNext();
     
@@ -44,8 +59,23 @@ int maxlen;
       case E_PRESS:
         keymap[ev.estruct.eparam] = 1;
         /* need to syntheise Ctrl / Shift ? */
-        *buffer++ = ev.estruct.eparam;
-        maxlen--;
+        ch = ev.estruct.eparam;
+        /* filter out modifier keys */
+        if (ch < 0x80)
+        {
+            if (keymap[0x88] || keymap[0x89])   /* shifted */
+            {
+                if (shifted[ch])
+                    ch = shifted[ch];
+            }
+
+            if (keymap[0x8a])                   /* control */
+            {
+                ch -= 0x60;
+            }
+
+            buffer[total++] = ch;
+        }
         break;
         
       case E_RELEASE:
@@ -60,4 +90,37 @@ int maxlen;
     }
   }
 
+  return total;
 }
+
+#ifdef TESTING
+
+int main(argc,argv)
+int argc;
+char **argv;
+{
+  int loop = 32;
+  int n;
+  unsigned char input[128];
+
+  EventEnable();
+  SetKBCode(0);
+  
+   while(loop > 0)
+   {
+     n = readkeyboardevents(input, 128);
+     if (n > 0)
+     {
+       printf("down: count(%d) 0x%02x\n", n, input[0]);
+       if(input[0] == 0x01)
+         break;
+       
+      loop--;
+     }   	
+   }	
+   
+   EventDisable();
+   SetKBCode(1);
+}
+#endif
+
