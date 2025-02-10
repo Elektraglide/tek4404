589fb833938733fd6cc7519e912f9d24da4c5614
Adam <adam@light-up.co.uk>
2025-02-10 13:53:53 +0000

    - added all known Uniflex configs

--- systat.c
+++ systat.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <sys/systat.h>
 
+char *confignames[] =
+{
+  "Unknown",
+  "Momentum_Hawk_32",
+  "PIXEL_100/AP",
+  "Tektronix_4404",
+  "TSC_SBC",
+  "Tektronix_CBI",
+  "Force_CPU-3V",
+  "VME/10",
+  "Ironics_IV1600",
+  "Gimix_GMX-20",
+  "NCR_Tower",
+  "Gimix_MICRO-20",
+  "SWTPc_SB68K",
+  "VME-1000",
+  "Tektronix_4406",
+  "Tektronix_4405",
+  "SWTPc_VME_System",
+  "Gimix_MICRO-20",
+  "Standard_VM"
+};
+
 main(argc,argv)
 int argc;
 char **argv;
@@ -12,6 +35,5 @@ int i;
 
   ptr = systat(&results);
   ss = (struct sstat *)ptr;
-  printf("config:%2.2x ver:%2.2x vendor:%2.2x memsize:%d\n",ss->ss_config, ss->ss_ver, ss->ss_vendor,ss->ss_memsiz);
-
+  printf("config:%s ver:%2.2x vendor:%2.2x memsize:%d\n",confignames[ss->ss_config], ss->ss_ver, ss->ss_vendor,ss->ss_memsiz);
 }
