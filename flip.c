e83ce75024ba7588df9e028ed6f529e2c44f8293
AdamB <adam@light-up.co.uk>
2025-02-18 19:26:42 +0000

    - render font into bitmap for sprite

--- flip.c
+++ flip.c
@@ -1,3 +1,4 @@
+#include <font.h>
 #include <graphics.h>
 #include <signal.h>
 
@@ -12,10 +13,13 @@ typedef long fixed;
 
 typedef struct
 {
+  struct FORM *form;
+  struct FORM *back[2];
   fixed x,y;
   int oldx[2],oldy[2];
 } sprite;
 
+struct FontHeader *font;
 struct FORM *screen;
 struct BBCOM bb;
 
@@ -39,19 +43,47 @@ void flip()
 void drawsprite(asprite)
 sprite *asprite;
 {
+  struct FORM *tmp;
+
+#if 0
 	bb.destrect.x = asprite->oldx[0];
     bb.destrect.y = asprite->oldy[0];
-	bb.destrect.w = 64;
+	bb.destrect.w = asprite->form->w;
     bb.destrect.h = 64;
 	bb.rule = bbZero;
 	BitBlt(&bb);
+#endif
+
+    bb.srcform = asprite->back[0];
+	bb.destrect.x = asprite->oldx[0];
+    bb.destrect.y = asprite->oldy[0];
+	bb.destrect.w = asprite->form->w;
+    bb.destrect.h = asprite->form->h;
+ 	bb.rule = bbSorD;
+	BitBlt(&bb);
     asprite->oldx[0] = asprite->oldx[1];
     asprite->oldy[0] = asprite->oldy[1];
+    tmp = asprite->back[0];
+    asprite->back[0] = asprite->back[1];
+    asprite->back[1] = tmp;
 
+    /* save under */
+    bb.srcform = screen;
+    bb.destform = asprite->back[1];
 	bb.destrect.x = FIX2INT(asprite->x);
     bb.destrect.y = FIX2INT(asprite->y) + (page.y);
-	bb.destrect.w = 64;
-    bb.destrect.h = 64;
+	bb.destrect.w = asprite->form->w;
+    bb.destrect.h = asprite->form->h;
+ 	bb.rule = bbS;
+	BitBlt(&bb);
+
+    /* draw over */
+    bb.srcform = asprite->form;
+    bb.destform = screen;
+	bb.destrect.x = FIX2INT(asprite->x);
+    bb.destrect.y = FIX2INT(asprite->y) + (page.y);
+	bb.destrect.w = asprite->form->w;
+    bb.destrect.h = asprite->form->h;
  	bb.rule = bbSorD;
 	BitBlt(&bb);
     asprite->oldx[1] = bb.destrect.x;
@@ -69,12 +101,45 @@ int sig;
   ESetAlarm(nexttime);
 }
 
+struct FORM *makesprite()
+{
+  struct POINT origin;
+  struct RECT r;
+  struct FORM *form;
+   
+   form = FormCreate(64,64);
+   
+   font = FontOpen("/fonts/PellucidaSans-Serif36.font");
+   if (font)
+   {
+     bb.destform = form;
+     bb.halftoneform = &BlackMask;
+     bb.destrect.x = 0;
+     bb.destrect.y = 0;
+     bb.destrect.w = form->w;
+     bb.destrect.h = form->h;
+
+     r.x = 5;
+     r.y = 5;
+     r.w = 20;
+     r.h = 40;
+     RectDrawX(&r, &bb);
+
+     origin.x = 0;
+     origin.y = 48;
+     StringDrawX("ABaj", &origin, &bb, font);
+   }
+
+   return form;
+}
+
 main(argc,argv)
 int argc;
 char *argv[];
 {
     register int ret, i;
     fixed x,y,dx,dy;
+    struct POINT origin;
 
     signal(SIGMILLI, flip);
 
@@ -110,17 +175,27 @@ char *argv[];
  	bb.rule = bbS;
 	BitBlt(&bb);
 
-    /* set up the bitblt command stuff */
-    bb.srcform = (struct FORM *)NULL;		/* no source */
-    bb.srcpoint.x = bb.srcpoint.y = 0;
-    bb.destform = screen;			/* dest is screen */
-
-
+   /* make a sprite to use */
+   thesprite.form = makesprite();
    thesprite.x = INT2FIX(20);
    thesprite.y = INT2FIX(10);
+
+   thesprite.back[0] = FormCreate(64,64);
+   thesprite.back[1] = FormCreate(64,64);
+
+    /* set up the bitblt command stuff */
+    bb.srcform = (struct FORM *)NULL;		/* no source */
+    bb.destform = screen;			/* dest is screen */
+    bb.destrect.x = 0;
+    bb.destrect.y = 0;
+    bb.destrect.w = 1024;
+    bb.destrect.h = 1024;
+    bb.halftoneform = &BlackMask;
+ 	bb.rule = bbS;
+
+   /* animate it */
    dx = INT2FIX(3);
    dy = INT2FIX(1);
-
    while(1)
    {
      drawsprite(&thesprite);
