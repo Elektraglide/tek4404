7fee26e498e7d90c08e94bce79f320330c2c77fa
AdamB <adam@light-up.co.uk>
2025-03-28 08:01:42 +0000

    - show just last 32 entries

--- last.c
+++ last.c
@@ -3,6 +3,10 @@
 #include <sys/fcntl.h>
 #include <sys/acct.h>
 
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
 int main(argc, argv)
 int argc;
 char **argv;
@@ -14,6 +18,12 @@ char **argv;
    fd = open("/act/history", O_RDONLY);	
   if (fd > 0)
   {
+    /* show last 32 enrties */
+  	n = lseek(fd, 0, SEEK_END);
+  	if (n > sizeof(record) * 32)
+ 	  n = sizeof(record) * 32;
+  	lseek(fd, -n, SEEK_CUR);
+  	
     while((n = read(fd, &record, sizeof(record))) > 0)
     {
       /* some records are init record */
@@ -24,7 +34,7 @@ char **argv;
       /* no CR */
       buffer[24] = '\0';
 
-      printf("%s tty%c%c\t%s\n", record.user_name,record.tty_num[0],record.tty_num[1], buffer);
+      printf("%10s tty%c%c\t%s\n", record.user_name,record.tty_num[0],record.tty_num[1], buffer);
     }
     close(fd);
   }
