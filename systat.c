fdb21244271acd60506ace356c3f4a96bb33029b
Adam <adam@light-up.co.uk>
2025-12-15 08:00:32 +0000

    - moved pagemonitor experiments to aseprate file

--- systat.c
+++ systat.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/systat.h>
-#include <sys/page_monitor.h>
 #include <signal.h>
 
 #include "kernutils.h"
@@ -30,23 +29,6 @@ char *confignames[] =
   "Standard_VM"
 };
 
-struct fault_information finfo;
-struct fault_information *fptr;
-
-void sh_fault(sig)
-int sig;
-{
-	/* unknown params; buffer, 10 (sizeof fault_information) */
-    pagemonitor(RETURN_READ_INFORMATION, finfo, sizeof(finfo));
-
-    printf("fault: %8.8x ",  finfo.fault_address);	
-    printf("%d\n", finfo.data_size);
-
-	signal(sig, sh_fault);	
-}
-
-int bigdata[1024];
-
 main(argc,argv)
 int argc;
 char **argv;
@@ -59,20 +41,6 @@ char *symbols;
 int symbolsize;
 char bootfile[32],buffer[32];
 struct fault_information *fresult;
-
-  memman(1, bigdata, bigdata+255);
-  printf("memman %d\n", errno);
-  
-  /* unknown params; start add, end addr?  */
-  fptr = pagemonitor(SET_READ_MONITOR, bigdata, bigdata+64);
-  printf("%8.8x\n", fptr);
-
-  signal(SIGRFAULT, sh_fault);
-  
-  /* do some reads */
-  for (i=0; i<1024; i+=128)
-    printf("bigdata = %d\n", bigdata[i]);
-
 
   kernbootfile(buffer);
   sprintf(bootfile, "/%s", buffer);
@@ -109,8 +77,5 @@ struct fault_information *fresult;
   printf("RAM_DISK: %d\n", getkconstant(symbols, symbolsize, "RAM_DISK_OPTION", pmem));
   printf("REAL_TIME: %d\n", getkconstant(symbols, symbolsize, "REAL_TIME_OPTION", pmem));
   printf("CLOCK Hz: %d\n", getkconstant(symbols, symbolsize, "CLOCKS_PER_SECOND", pmem));
-
-  printf("cleanup\n");
-  pagemonitor(CLEAR_READ_MONITOR, 0, 16);
   
 }
