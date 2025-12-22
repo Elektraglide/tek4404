b0e20dd502b42c88c629ebab35e55cad1f5d763c
AdamB <adam@light-up.co.uk>
2025-12-22 09:00:35 +0000

    - fix call tosystem_control()

--- dbg.c
+++ dbg.c
@@ -7,10 +7,22 @@
 #include <sys/pty.h>
 #include <sys/sgtty.h>
 
+#include <varargs.h>
 #include <net/socket.h>
 #include <net/in.h>
 
+#define CODEWIN 10
+#define CMDWIN 23
+
+typedef struct
+{
+    short y, h;
+    char back[80 * 8];
+} retainedwin;
+
+
 char buffer[4096];
+retainedwin regwindow;
 
 int sockpair[2] = { 0,0 };
 int readerpid = 0;
@@ -108,7 +120,7 @@ char* argv1;
 
 #ifndef __clang__
         /* calls pty_make_controlling_terminal */
-        system_control(2);
+        system_control(2, fdslave);
 #endif
 
         /* As the child is a session leader, set the controlling terminal to be the slave side of the PTY */
@@ -141,11 +153,39 @@ void closedown()
   exit(0);
 }
 
+int logit(_varargs)
+int _varargs;
+{
+    va_list p;
+    char* fmt;
+    unsigned int val1;
+    unsigned int val2;
+    unsigned int val3;
+    unsigned int val4;
+
+    va_start(p);
+    fmt = va_arg(p, char*);
+
+    val1 = va_arg(p, unsigned int);
+    val2 = va_arg(p, unsigned int);
+    val3 = va_arg(p, unsigned int);
+    val4 = va_arg(p, unsigned int);
+    va_end(p);
+
+    printf("\033[25;33r\033[8;1H");
+    printf(fmt, val1, val2, val3, val4);
+
+    /* we dont know what is the current window, so.. */
+    printf("\033[1;20r");
+
+    return 0;
+}
+
+
 int sighandler(sig)
 int sig;
 {
-    printf("\033[1;32r\033[31;1H");
-    fprintf(stderr, "closing down\n");
+    logit("\033[1;33rclosing down\n");
     dbgcleanup(sig);
     closedown();
 
@@ -216,18 +256,128 @@ int buflen;
     return total;
 }
 
-printclear(buf)
+printdiffclear(win, buf)
+retainedwin* win;
 char* buf;
 {
-    char *ptr,*eol;
+    char *srcbeg,*dstbeg;
+    short i;
+    int linenum;
+
+    linenum = win->y;
+
+    srcbeg = buf;
+    dstbeg = win->back;
+
+    while (linenum < win->h)
+    {
+        char* src;
+        char* dst;
+        char* srcend;
+        char* dstend;
+        char* startsrc;
+        short inside_run;
+
+        srcend = strchr(srcbeg, '\n');
+        dstend = strchr(dstbeg, '\n');
+
+        /*stop if no line */
+        if (!srcend)
+            break;
+
+        if (!dstend)
+        {
+            dstend = dstbeg + (srcend - srcbeg);
+        }
+
+        /* compare pair of lines */
+        src = srcbeg;
+        dst = dstbeg;
+        inside_run = 0;
+        while (src < srcend)
+        {
+            char c, d;
+            int offset;
+
+            c = *src++;
+            d = *dst++;
+
+            if (c != d)
+            {
+                if (inside_run == 0)
+                {
+                    startsrc = src-1;
+                    inside_run = 1;
+                }
+            }
+            else
+            if (inside_run)
+            {
+                c = src[0];
+                src[0] = '\0';
+                offset = startsrc - srcbeg + 1;
+                printf("\033[%d;%dH%s", linenum, offset, startsrc);
+                src[0] = c;
+
+                inside_run = 0;
+            }
+        }
+
+        if (inside_run)
+        {
+            char c;
+            int offset;
+
+            c = src[0];
+            src[0] = '\0';
+            offset = startsrc - srcbeg + 1;
+            printf("\033[%d;%dH%s", linenum, offset, startsrc);
+            src[0] = c;
+        }
+
+        /* if its shorter, clear out old */
+        if (dst < dstend)
+            printf("\033[0K");
+
+        /* start of next lines */
+        srcbeg = srcend + 1;
+        dstbeg = dstend + 1;
+        linenum++;
+    }
+
+    strcpy(win->back, buf);
+}
+
+printclear(buf, n)
+char* buf;
+int n;
+{
+    char *ptr;
 
     ptr = strtok(buf, "\n");
-    while (ptr)
+    while (--n >= 0 && ptr)
     {
         printf(ptr);
         printf("\033[0K\n");
         ptr = strtok(NULL, "\n");
     }
+}
+
+printlastclear(buf, n)
+char* buf;
+int n;
+{
+    char* ptr;
+    char* curr;
+
+    curr = ptr = strtok(buf, "\n");
+    while (--n > 0 && ptr)
+    {
+        curr = ptr;
+        ptr = strtok(NULL, "\n");
+    }
+    printf(curr);
+    printf("\033[0K");
 }
 
 main(argc, argv)
@@ -238,6 +388,7 @@ char **argv;
 	int rc;
 	char *ptr, ch, input[32];
     int master, slave;
+    int refresh;
 
     signal(SIGINT, sighandler);
 
@@ -252,24 +403,48 @@ char **argv;
   master = ptfd[1];
   slave = ptfd[0];
 
-  printf("\033[2J\033[24;1H");
+  printf("\033[1;32r\033[2J\033[%d;1H",CMDWIN-1);
   printf("===== Dbg v1.0 %s [pid:%d] =====\n", argv[1], readerpid);
-  sleep(2);
+  sleep(1);
   rc = readdebug(master, buffer, sizeof(buffer));
+  printf("\033[32;1H");
   printf(buffer);
- 
+
+  /* retained window for reg display */
+  regwindow.y = 1;
+  regwindow.h = 7;
+
+  refresh = 1;
   while (1)
   {
-      /* make a 8 line scroll region */
-      printf("\033[25;33r\033[8;1H>"); fflush(stdout);
+      /* make a 10 line scroll region */
+      printf("\033[%d;%dr\033[10;1H>", CMDWIN, CMDWIN+10);
+      fflush(stdout);
       rc = (int)read(0, input, sizeof(input));
-      ch = input[0];
-	  if (ch == 'x')
+      if (rc > 1) ch = input[0];
+      if (ch == '?')
+      {
+          printf("x - create process\n");
+          printf("s - singlestep\n");
+          printf("g - go\n");
+          printf("t - trace\n");
+          printf("q - quit\n");
+      }
+      if (ch == 'q')
+      {
+          sighandler(0);
+
+          write(master, "q\n", 2);
+          readdebug(master, buffer, sizeof(buffer));
+          printf(buffer);
+      }
+      if (ch == 'x')
 	  {
 			write(master, "x\n", 2);
             readdebug(master, buffer, sizeof(buffer));
 			printf(buffer);
-	  }
+            refresh = 1;
+      }
 	  if (ch == 'g')
 	  {
 		  write(master, "g\n", 2);
@@ -280,7 +455,7 @@ char **argv;
       {
           write(master, "s\n", 2);
           readdebug(master, buffer, sizeof(buffer));
-          printf(buffer);
+          /* printf(buffer); */
       }
       if (ch == 't')
       {
@@ -299,16 +474,37 @@ char **argv;
 	  /* refresh top of screen */
       printf("\033[1;20r\033[1;1H");
 
+      /* get register state */
 	  write(master, "r\n", 2);
 	  rc = readdebug(master, buffer, sizeof(buffer));
-      printclear(buffer);
+      if (refresh)
+      {
+          memcpy(regwindow.back, buffer, sizeof(regwindow.back));
+          printclear(regwindow.back, 5);
+      }
+      else
+      {
+          printdiffclear(&regwindow, buffer);
+      }
 
-      printf("\033[7;1H"); 
-      
-      write(master, "i .-0,16\n", 9);
-	  rc = readdebug(master, buffer, sizeof(buffer));
-	  printclear(buffer);
+      /* get disassembly (more than we need because we want N line) */
+      write(master, "i .-0,20\n", 9);
+      rc = readdebug(master, buffer, sizeof(buffer));
+      /* full redraw or 1 line redraw */
+      if (1 || refresh)
+      {
+          printf("\033[%d;1H",CODEWIN);
+          printclear(buffer, 7);
+      }
+      else
+      {
+          /* scroll region 1 line */
+          printf("\033[%d;%dr\033[17;1H",CODEWIN,CODEWIN+7);
+          printlastclear(buffer, 8);
+          printf("\n");
+      }
 
+      refresh = 0;
   }
   
    /* restore */
