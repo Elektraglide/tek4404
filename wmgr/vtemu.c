99da52c9bc9804197f500f51890dcbb4d9a1e5b8
Adam <adam@light-up.co.uk>
2026-04-19 12:52:44 +0100

    - scrolling off by 1

--- wmgr/vtemu.c
+++ wmgr/vtemu.c
@@ -178,8 +178,8 @@ int fastscroll;
   {
     vt->cy -= numlines;
     /* scroll up */
-    VTmovelines(vt, vt->margintop, vt->margintop+numlines, vt->marginbot - vt->margintop - numlines);
-    VTclearlines(vt,vt->marginbot-numlines,numlines);
+    VTmovelines(vt, vt->margintop, vt->margintop+numlines, vt->marginbot - vt->margintop + 1 - numlines);
+    VTclearlines(vt,vt->marginbot,numlines);
   }
   
   vt->dirtylines |= (1<<vt->cy);
