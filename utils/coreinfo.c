d6cb8a6411652cac26fac336f14d33ffffb76dd2
Adam <adam@light-up.co.uk>
2026-05-19 19:11:05 +0200

    - fixed dumping registers - fixed dumping current task - file descriptors unknown

--- utils/coreinfo.c
+++ utils/coreinfo.c
@@ -6,6 +6,25 @@
 #include "uniflex/task.h"
 #include "uniflex/userbl.h"
 
+#ifdef __clang__
+#include <unistd.h>
+#include <string.h>
+#include <sys/_types/_u_char.h>
+#include <sys/_types/_u_short.h>
+#include <sys/_types/_u_int.h>
+#include <netinet/ip.h>
+#else
+
+
+#endif
+
+
+/* S_IFDIR block table, 32 per block */
+struct dirblk {
+	unsigned short d_fdn;
+	char d_name[14];
+};
+
 int header[1024];
 
 int main(argc,argv)
@@ -15,56 +34,104 @@ char **argv;
 	int fd;
 	int i;
 
-	fd = open("core", O_RDONLY);
+	fd = open(argc > 1 ? argv[1] : "core", O_RDONLY);
 	if (fd > 0)
 	{
 		struct userbl* userbl;
-		struct task* task;
+		struct task atask;
 		struct mt* map;
 		unsigned int* regs;
-
+		unsigned char buffer[128],utmat[128];
+		char *status;
+		long timestamp;
+		
 		/* first 4k */
 		read(fd, header, sizeof(header));
 
-		userbl = (struct userbl *)(((char*)header) + header[0]);
+		userbl = (struct userbl *)(((char*)header) + ntohl(header[0]));
 
-		printf("utimu  %8.8x\n", userbl->utimu);
-		printf("uregs  %8.8x\n", userbl->uregs);
-		printf("utask  %8.8x\n", userbl->utask);
+	timestamp = userbl->ustart;
+	printf("task start: %2.2d:%2.2d:%2.2d \n", (timestamp/3600), (timestamp%3600) / 60, (timestamp%3600) % 60);
 
-		map = (struct mt *)userbl->umem;
-		while (map->numpages > 0)
+		i = 1 + ntohs(userbl->usizet) + ntohs(userbl->usized) + ntohs(userbl->usizes);
+		lseek(fd, i * 4096, SEEK_SET);
+		read(fd, &atask, sizeof(atask));
+				
+		// reading VM page map?
+		read(fd, &utmat, 0x1a);
+		printf("umemc %d\n", ntohs(userbl->umemc));
+		// based on task size, writes more or less
+		i = ntohs(userbl->umemc) ;
+		read(fd, buffer, 0x80 << (i & 0x3f));
+		read(fd, buffer, 0x80 << (i & 0x3f));
+
+		// reading open files
+		for(i=0; i<UNFILS; i++)
 		{
-			printf("%8.8x - %8.8x\n", map->vaddr, map->vaddr + map->numpages * 4096 - 1);
+			if (userbl->ufiles[i])
+			{
+					read(fd, buffer, 0xc);
+					read(fd, buffer, 0x5e);
+			}
+		}
+		
+		printf("uid(%d) uuid(%d) pid(%d) ppid(%d)\n", ntohs(atask.tsuid), ntohs(userbl->uuid), ntohs(atask.tstid),ntohs(atask.tstidp));
+
+		status = "";
+		if (atask.tsstat == TRUN)  status = "RUN";
+		if (atask.tsstat == TSLEEP) status = "SLEEP";
+		if (atask.tsstat == TWAIT) status = "WAIT";
+		if (atask.tsstat == TTERM) status = "TERM";
+		if (atask.tsstat == TTRACE) status = "TRACE";
+		printf("tsstat(%d) %s\n", atask.tsstat, status);
+
+		buffer[0] = '\0';
+		if (atask.tsmode & TCORE) strcat(buffer, "CORE ");
+		if (atask.tsmode & TLOCK) strcat(buffer, "LOCK ");
+		if (atask.tsmode & TSYSTM) strcat(buffer, "SYSTM ");
+		if (atask.tsmode & TTRACP) strcat(buffer, "TRACE ");
+		if (atask.tsmode & TSWAPO) strcat(buffer, "SWAP ");
+		if (atask.tsmode & TARGX) strcat(buffer, "ARGX ");
+		printf("tsmode(%d) %s\n", atask.tsmode, buffer);
+		printf("utimu(%d) utims(%d)\n", ntohl(userbl->utimu),ntohl(userbl->utims));
+
+		
+		printf("map:\n");
+		map = (struct mt *)userbl->umem;
+		while (ntohs(map->numpages) > 0)
+		{
+			printf("%8.8x - %8.8x\n", ntohl(map->vaddr), ntohl(map->vaddr) + ntohs(map->numpages) * 4096 - 1);
 			map++;
 		}
+		if (userbl->udep_chunks > 0)
+		{
+			printf("extended map:\n");
+			map = (struct mt *)userbl->umdep_segs;
+			while (ntohs(map->numpages) > 0)
+			{
+				printf("%8.8x - %8.8x\n", ntohl(map->vaddr), ntohl(map->vaddr) + ntohs(map->numpages) * 4096 - 1);
+				map++;
+			}
+		}
 
-		regs = (unsigned int *)((char*)userbl - 66 - (16 * 4));
+		/* relies on userbl being on a 4k boundary.. */
+		printf("registers:\n");
+		regs =  (unsigned int *)((char*)header + (ntohl(userbl->uregs) & 0xfff));
 		printf("Dn ");
 		for (i = 0; i < 8; i++)
-			printf("%8.8x ", regs[i]);
+			printf("%8.8x ", ntohl(regs[i]));
 		printf("\n");
 		printf("An ");
 		for (i = 0; i < 8; i++)
-			printf("%8.8x ", regs[8+i]);
+			printf("%8.8x ", ntohl(regs[8+i]));
 		printf("\n");
 
-printf("offset = %8.8x\n", header[0] + (char *)&userbl->usizet - (char *)userbl);
-
-printf("%d %d %d\n",userbl->usizet, userbl->usized, userbl->usizes);
-
-		i = 1 + userbl->usizet + userbl->usized + userbl->usizes;
-		lseek(fd, i * 4096, SEEK_SET);
-		lseek(fd, 4, SEEK_CUR);
-		read(fd, &task, sizeof(task));
-		printf("tsuid  %d\n", task->tsuid);
-		printf("tstid  %d\n", task->tstid);
-		printf("tstidp  %d\n", task->tstidp);
-		printf("tsalrm  %d\n", task->tsalrm);
-		printf("tsdatp  %d\n", task->tsdatp);
-		printf("tsstkp  %d\n", task->tsstkp);
-
-		printf("tsstat  %d\n", task->tsstat);
+		printf("bin flags: 0x%4.4x\n", ntohs(userbl->ubin_flags));
+		
+		struct dirblk *fdn = (struct dirblk *)userbl->ufdn;
+		printf("cwd entry: \"%s\"\n", fdn->d_name);
+		
+		close(fd);
 	}
 }
 
