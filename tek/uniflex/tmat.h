b5b15a3306e1d61b6d14eb71c35b6cd944cf4ead
Adam <adam@light-up.co.uk>
2026-07-21 19:32:12 +0200

    - use ptr32 typedef

--- tek/uniflex/tmat.h
+++ tek/uniflex/tmat.h
@@ -3,11 +3,11 @@
 struct tmat
 {
 	ushort unknown1;
-	ulong memqueue;		/* 0x02 */
-	ulong unknown2;
-	ulong pooledpages;	/* 0x0a */
-	ulong privpages;	/* 0x0e */
+	ptr32 memqueue;		/* 0x02 */
+	ptr32 unknown2;
+	ptr32 pooledpages;	/* 0x0a */
+	ptr32 privpages;	/* 0x0e */
 	ushort tasksize; 	/* 0x12 */
 	ushort taskordinal;	/* 0x14 */	
-	ulong unknown3;		/* 0x16 */
+	ptr32 unknown3;		/* 0x16 */
 };
